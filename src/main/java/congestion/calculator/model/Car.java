1c3ec4cae799a86863c732743b8cf3b0e949f25c
Mikael Andersson Wigander <mikael.andersson.wigander@cssregtech.com>
2022-05-27 10:01:09 +0200

    :tada: Initial commit.

---
+++ src/main/java/congestion/calculator/model/Car.java
@@ -0,0 +1,8 @@
+package congestion.calculator.model;
+
+public class Car implements Vehicle {
+
+    public String getVehicleType() {
+        return "Car";
+    }
+}
---
+++ src/main/java/congestion/calculator/model/Motorbike.java
@@ -0,0 +1,8 @@
+package congestion.calculator.model;
+
+public class Motorbike implements Vehicle {
+    @Override
+    public String getVehicleType() {
+        return "Motorbike";
+    }
+}
